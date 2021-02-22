7a94519851e67f640f939ef437b1bbe897086422
andrew <20448589@life.hkbu.edu.hk>
2021-02-22 18:38:02 +0800

    create database

--- chatbot_data.py
+++ chatbot_data.py
@@ -10,21 +10,23 @@ Instead you want to build up a big transaction
 just do it all at once
 '''
 sql_transaction = []
-connection = sqlite3.connect('{}.db'.format(timeframe))
+connection = sqlite3.connect('./data/{}.db'.format(timeframe))
 cursor = connection.cursor()
 
 def create_table():
     cursor.execute("""CREATE TABLE IF NOT EXISTS parent_reply
-    (parent_id TEXT PRIMARY, comment_id TEXT UNIQUE, 
+    (parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, 
     parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)""")
 
 '''
+WHY NEED 
 Subreddit:
 Different Subreddit may have different types of talking style
 '''
 
 if __name__ == '__main__':
     create_table()
+
 
 
 
